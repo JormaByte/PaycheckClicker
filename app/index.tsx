f798041fa57ee1e9c59c6993d1b73740d6f9dff4
JormaByte <joel.oksanen1223@gmail.com>
2024-05-11 16:10:51 +0300

    basic functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View, TextInput, StyleSheet } from "react-native";
 
 export default function Index() {
+
+  let [rate, setRate] = useState(0)
+  let [moneyMade, setMoneyMade] = useState(0)
+
+  const addMoney = () => {
+    setMoneyMade(moneyMade + rate)
+  }
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={Styles.View}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome to the Paycheck Clicker! </Text>
+      <Text>- helping you stay motivated :)</Text>
+
+      <Pressable style={Styles.Pressable} onPress={addMoney}><Text>{rate}</Text></Pressable>
+
+      <TextInput
+        value={rate.toString()}
+        placeholder="Change your daily rate"
+        keyboardType="numeric"
+        onChangeText={(text) => setRate(parseInt(text))}
+      />
+
+      <Text>TOTAL MONEY MADE SO FAR:</Text>
+      <Text>{moneyMade} €</Text>
     </View>
+
   );
 }
+
+const Styles = StyleSheet.create({
+  View: {
+    flex: 1,
+    alignItems: "center",
+  },
+  Pressable: {
+    borderWidth: 1,
+    borderRadius: 100,
+    padding: 20,
+    margin: 20
+  }
+})
